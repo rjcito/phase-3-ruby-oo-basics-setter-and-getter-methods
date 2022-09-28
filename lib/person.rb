38f2305f4ff75dce4ce9f67490e25773da3665aa
Roberto Jacquez <rjqz92@gmail.com>
2022-09-28 15:16:01 -0600

    commit tree 83a82f42ff2a0fcb7e4c110688af8ee33911fa82

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    # setter method
+    def name=(name)
+        @name= name
+    end
+    # getter method
+    def name
+        @name
+    end
 
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 end
